3f38396404fa42d41514c7f857a5f4f0fe031683
Harrison.Ding <dinglinbin@gmail.com>
2018-08-05 11:52:12 +0800

    Integrate with MyBatis: 1.3.2, and valid with user successfully. usage: http://localhost:8080/getuser?name=Harrison-1533392050650

--- src/main/java/hd/spring/boot/web/controllers/UserController.java
+++ src/main/java/hd/spring/boot/web/controllers/UserController.java
@@ -2,19 +2,32 @@ package hd.spring.boot.web.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import hd.spring.boot.web.mapper.UserMapper;
+import hd.spring.boot.web.model.UserEntity;
 import hd.spring.boot.web.service.UserServiceI;
 
 @RestController
 public class UserController {
 
 	@Autowired
-	private UserServiceI userService;
+	private UserServiceI	userService;
+
+	@Autowired
+	private UserMapper		userMapper;
 
 	@RequestMapping("/restIndex")
 	public String restIndex() {
 		userService.createUser("Harrison-" + System.currentTimeMillis(), 18);
 		return "success";
+	}
+
+	// http://localhost:8080/getuser?name=Harrison-1533392050650
+	@ResponseBody
+	@RequestMapping("/getuser")
+	public UserEntity getUser(String name) {
+		return userMapper.findUser(name);
 	}
 }
